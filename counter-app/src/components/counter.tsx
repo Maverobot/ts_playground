f7d62c7af5c2c641b008389a8d1d2fcb97627d4d
Maverobot <quzhengrobot@gmail.com>
2020-10-30 19:58:57 +0100

    added: counter-app

---
+++ counter-app/src/components/counter.tsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CSS from 'csstype';
+
+interface CounterProps {}
+
+interface CounterState {
+  count: number;
+  tags: string[];
+}
+
+class Counter extends Component<CounterProps, CounterState> {
+  styles: CSS.Properties = {
+    fontSize: '20px',
+    color: 'blue',
+    fontWeight: 'bold',
+  };
+
+  constructor(props: CounterProps) {
+    super(props);
+    this.state = { count: 0, tags: ['tag1', 'tag2', 'tag3'] };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button className="btn btn-secondary btn-sm"> Increment </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes: string = 'badge m-2 badge-';
+    classes = classes.concat(this.state.count === 0 ? 'warning' : 'primary');
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+}
+
+export default Counter;
