305f29e682ba373168260e20f2ab020729a248f5
Zheng Qu <quzhengrobot@gmail.com>
2020-11-01 16:52:53 +0100

    react basics: stateless functinal component

--- counter-app/src/components/navbar.tsx
+++ counter-app/src/components/navbar.tsx
@@ -4,17 +4,18 @@ interface Props {
   totalCounters: number;
 }
 
-export default class NavBar extends Component<Props> {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill badge-secondary m-2">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// stateless functional component
+const NavBar = (props: Props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary m-2">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
