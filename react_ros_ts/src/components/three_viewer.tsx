9cda7dda1e29cfdb1de39d3579f8549a1fcc205f
Maverobot <quzhengrobot@gmail.com>
2020-11-19 10:58:42 +0100

    threejs: keep camera aspect ratio

--- react_ros_ts/src/components/three_viewer.tsx
+++ react_ros_ts/src/components/three_viewer.tsx
@@ -11,18 +11,19 @@ class ThreeViewer extends Component<Props> {
     super(props);
 
     this.renderer = new THREE.WebGLRenderer();
-  }
-
-  renderer: THREE.WebGLRenderer;
-
-  componentDidMount() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(
+    this.camera = new THREE.PerspectiveCamera(
       75,
       this.props.width / this.props.height,
       0.1,
       1000
     );
+  }
+
+  renderer: THREE.WebGLRenderer;
+  camera: THREE.PerspectiveCamera;
+
+  componentDidMount() {
+    let scene = new THREE.Scene();
     this.renderer.setSize(this.props.width, this.props.height);
     let container = document.getElementById('canvas');
     if (!container) {
@@ -35,19 +36,22 @@ class ThreeViewer extends Component<Props> {
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 5;
+    this.camera.position.z = 5;
 
     let rendererRef = this.renderer;
-    var animate = () => {
+    let cameraRef = this.camera;
+    let animate = () => {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      rendererRef.render(scene, camera);
+      rendererRef.render(scene, cameraRef);
     };
     animate();
   }
 
   componentDidUpdate() {
+    this.camera.aspect = this.props.width / this.props.height;
+    this.camera.updateProjectionMatrix();
     this.renderer.setSize(this.props.width, this.props.height);
   }
 
